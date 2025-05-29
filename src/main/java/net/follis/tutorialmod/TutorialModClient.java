a5cee534d3914d4eb5361f9b532508499ae30cf3
FollisNow <72704174+FollisNow@users.noreply.github.com>
2025-05-30 00:01:41 +0200

    Initial commit

---
+++ src/main/java/net/follis/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.follis.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
