a736abe17b1a03f9de7be52240fe42cc61bc3434
FollisNow <72704174+FollisNow@users.noreply.github.com>
2025-05-30 12:23:41 +0200

    basic items and blocks

--- src/main/java/net/follis/tutorialmod/TutorialMod.java
+++ src/main/java/net/follis/tutorialmod/TutorialMod.java
@@ -2,6 +2,9 @@ package net.follis.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.follis.tutorialmod.block.ModBlocks;
+import net.follis.tutorialmod.item.ModItemGroups;
+import net.follis.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +16,10 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
 	}
 }
