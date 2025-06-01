db739a4208249d152dc8907ef2a2d85ca4745e14
FollisNow <72704174+FollisNow@users.noreply.github.com>
2025-06-01 16:38:49 +0200

    added pink garnet tools and recipes

--- src/main/java/net/follis/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/follis/tutorialmod/datagen/ModRecipeProvider.java
@@ -11,8 +11,6 @@ import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.*;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
