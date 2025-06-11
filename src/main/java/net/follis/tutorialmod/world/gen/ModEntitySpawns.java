60c1f54a8d7ff02fbeb99a8ca17606ed8f35b6b3
FollisNow <72704174+FollisNow@users.noreply.github.com>
2025-06-11 19:29:45 +0200

    added mantis world spawn

--- src/main/java/net/follis/tutorialmod/world/gen/ModEntitySpawns.java
+++ src/main/java/net/follis/tutorialmod/world/gen/ModEntitySpawns.java
@@ -4,7 +4,6 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.follis.tutorialmod.entity.ModEntities;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.SpawnLocation;
 import net.minecraft.entity.SpawnLocationTypes;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.passive.AnimalEntity;
