2b6f4e75d5af97cfa67512c2b952fa90db145cba
FollisNow <72704174+FollisNow@users.noreply.github.com>
2025-07-28 21:10:40 +0200

    GoldenLeaves now drops gold nuggets randomly

--- src/main/java/net/follis/tutorialmod/block/custom/GoldenLeavesBlock.java
+++ src/main/java/net/follis/tutorialmod/block/custom/GoldenLeavesBlock.java
@@ -3,23 +3,16 @@ package net.follis.tutorialmod.block.custom;
 import com.mojang.serialization.MapCodec;
 import net.follis.tutorialmod.particle.ModParticles;
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleUtil;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 public class GoldenLeavesBlock extends LeavesBlock {
     public static final MapCodec<GoldenLeavesBlock> CODEC = createCodec(GoldenLeavesBlock::new);
