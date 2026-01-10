886e4e3857c37b0902cfc56d779ff365a6a81a86
FollisNow <72704174+FollisNow@users.noreply.github.com>
2026-01-10 12:57:53 +0000

    flowerpot for golden sapling

--- src/main/java/net/follis/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/follis/tutorialmod/block/ModBlocks.java
@@ -11,7 +11,6 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.TallBlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -266,11 +265,6 @@ public class ModBlocks {
     private static void registerBlockItem(String name, Block block) {
         Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),
                 new BlockItem(block, new Item.Settings()));
-    }
-
-    private static void registerTallBlockItem(String name, Block block) {
-        Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),
-                new TallBlockItem(block, new Item.Settings()));
     }
 
     public static void registerModBlocks() {
