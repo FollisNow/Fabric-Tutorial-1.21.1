fbcf7d3cb0e544dde260fa01d8563935460f0219
FollisNow <72704174+FollisNow@users.noreply.github.com>
2026-04-20 21:02:55 +0100

    added chitin drop in all bugs

--- src/main/java/net/follis/tutorialmod/util/ModLootTableModifiers.java
+++ src/main/java/net/follis/tutorialmod/util/ModLootTableModifiers.java
@@ -16,11 +16,14 @@ import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.entity.EntityTypePredicate;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.EntityTypeTags;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Debug;
 
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +34,52 @@ import java.util.stream.Collectors;
 public class ModLootTableModifiers {
     private static final Identifier GRASS_BLOCK_ID
             = Identifier.of("minecraft", "blocks/short_grass");
+    private static final Identifier BEE_ID
+            = Identifier.of("minecraft", "entities/bee");
+    private static final Identifier BLAZE_ID
+            = Identifier.of("minecraft", "entities/blaze");
+    private static final Identifier ENDERMITE_ID
+            = Identifier.of("minecraft", "entities/endermite");
+    private static final Identifier SILVERFISH_ID
+            = Identifier.of("minecraft", "entities/silverfish");
+    private static final Identifier SPIDER_ID
+            = Identifier.of("minecraft", "entities/spider");
+    private static final Identifier CAVE_SPIDER_ID
+            = Identifier.of("minecraft", "entities/cave_spider");
+    public static final Identifier AMETHYST_BEE_ID =
+            Identifier.of(TutorialMod.MOD_ID, "amethyst_bee");
+    public static final Identifier BEETLE_ID =
+            Identifier.of(TutorialMod.MOD_ID, "beetle");
+    public static final Identifier SPIDERLING_ID =
+            Identifier.of(TutorialMod.MOD_ID, "spiderling");
+    public static final Identifier LOCUST_ID =
+            Identifier.of(TutorialMod.MOD_ID, "locust");
+    public static final Identifier MANTIS_ID =
+            Identifier.of(TutorialMod.MOD_ID, "mantis");
+    public static final Identifier MOTH_ID =
+            Identifier.of(TutorialMod.MOD_ID, "moth");
+    public static final Identifier SCORPION_ID =
+            Identifier.of(TutorialMod.MOD_ID, "scorpion");
+    public static final Identifier GOLDEN_SILVERFISH_ID =
+            Identifier.of(TutorialMod.MOD_ID, "golden_silverfish");
+
+    private static final List<Identifier> ARTHROPODS = List.of(
+            BEE_ID,
+            BLAZE_ID,
+            ENDERMITE_ID,
+            SILVERFISH_ID,
+            SPIDER_ID,
+            CAVE_SPIDER_ID,
+
+            AMETHYST_BEE_ID,
+            BEETLE_ID,
+            SPIDERLING_ID,
+            LOCUST_ID,
+            MANTIS_ID,
+            MOTH_ID,
+            SCORPION_ID,
+            GOLDEN_SILVERFISH_ID
+    );
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
@@ -62,21 +111,24 @@ public class ModLootTableModifiers {
                 tableBuilder.pool(poolBuilder.build());
             }
 
-            if (Registries.ENTITY_TYPE.get(key.getValue()).isIn(EntityTypeTags.ARTHROPOD)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(1.0f))
-                        .with(ItemEntry.builder(ModItems.CHITIN))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
-                tableBuilder.pool(poolBuilder.build());
-            }
-
-            AddItemLootTo(ModEntities.GOLDEN_SILVERFISH.getLootTableId().getValue(), key, tableBuilder, Items.GOLD_NUGGET);
+            AddItemLootTo(ARTHROPODS, key, tableBuilder, ModItems.CHITIN);
+            AddItemLootTo(GOLDEN_SILVERFISH_ID, key, tableBuilder, Items.GOLD_NUGGET);
         });
     }
 
+    private static void AddItemLootTo(List<Identifier> identifiers, RegistryKey<LootTable> key, LootTable.Builder tableBuilder, Item item) {
+        for (Identifier identifier: identifiers) {
+            if (identifier.equals(key.getValue())){
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1.0f))
+                        .with(ItemEntry.builder(item))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        }
+    }
     private static void AddItemLootTo(Identifier identifier, RegistryKey<LootTable> key, LootTable.Builder tableBuilder, Item item) {
-
         if (identifier.equals(key.getValue())){
             LootPool.Builder poolBuilder = LootPool.builder()
                     .rolls(ConstantLootNumberProvider.create(1))
